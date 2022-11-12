ff9ff10f690481d94626deed4c92eb6d29584bb1
simoneliasen <simoneliasen@gmail.com>
2022-11-12 21:58:48 +0100

    removed metric.py

--- train.py
+++ train.py
@@ -12,8 +12,7 @@ import torch.nn as nn
 
 from config import build_parser
 from model import Transformer
-from data_loader import Dataset_ETT_hour, Dataset_ETT_minute
-from metrics import metric
+from data_loader import Dataset_ETT_hour
 
 import matplotlib.pyplot as plt
 
@@ -73,6 +72,38 @@ def _get_data(args, flag):
 
     return data_set, data_loader
 
+
+def RSE(pred, true):
+    return np.sqrt(np.sum((true-pred)**2)) / np.sqrt(np.sum((true-true.mean())**2))
+
+def CORR(pred, true):
+    u = ((true-true.mean(0))*(pred-pred.mean(0))).sum(0) 
+    d = np.sqrt(((true-true.mean(0))**2*(pred-pred.mean(0))**2).sum(0))
+    return (u/d).mean(-1)
+
+def MAE(pred, true):
+    return np.mean(np.abs(pred-true))
+
+def MSE(pred, true):
+    return np.mean((pred-true)**2)
+
+def RMSE(pred, true):
+    return np.sqrt(MSE(pred, true))
+
+def MAPE(pred, true):
+    return np.mean(np.abs((pred - true) / true))
+
+def MSPE(pred, true):
+    return np.mean(np.square((pred - true) / true))
+
+def metric(pred, true):
+    mae = MAE(pred, true)
+    mse = MSE(pred, true)
+    rmse = RMSE(pred, true)
+    mape = MAPE(pred, true)
+    mspe = MSPE(pred, true)
+    
+    return mae,mse,rmse,mape,mspe
 
 def run_metrics(caption, preds, trues):
     preds = np.array(preds)
